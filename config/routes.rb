75661d6b87149a3a78cd9e182cc82bd6317f9f4a
Ika Wastiningsih <ikawastiningsih@gmail.com>
2020-10-25 20:04:52 +0700

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :schedule_users
+  resources :schedules
+  resources :hospitals
+  resources :doctors
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
